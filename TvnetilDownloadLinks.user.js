47b51f074d552f860d7ffb496069740f27512bd6
RealGame <realgam3@gmail.com>
2014-08-30 02:25:17 +0300

    TvnetilDownloadLinks v1.2

--- TvnetilDownloadLinks.user.js
+++ TvnetilDownloadLinks.user.js
@@ -4,21 +4,20 @@
 // @author      RealGame (Tomer Zait)
 // @description Get FaveZ0ne Download Links On Tvnetil Review Page
 // @include     /^http(s)?://(www\.)?tvnetil\.net/review/\d+/\d+/$/
-// @version     1.1
+// @version     1.2
 // @grant       GM_xmlhttpRequest
 // ==/UserScript==
+// Add replaceAll To The String Prototype
+String.prototype.replaceAll = function(search, replacement) {
+    return this.split(search).join(replacement);
+};
+
+// Add remove To The Element Prototype
+Element.prototype.remove = function() {
+    this.parentElement.removeChild(this);
+};
+
 (function() {
-    // Add replaceAll To The String Prototype
-    String.prototype.replaceAll = function(search, replacement) {
-        var target = this;
-        return target.split(search).join(replacement);
-    };
-
-    // Add remove To The Element Prototype
-    Element.prototype.remove = function() {
-        this.parentElement.removeChild(this);
-    };
-
     // EncodeURIComponent With Windows-1255 Special Characters Encoding
     function encodeWin1255URIComponent(str) {
         var win1255EncodedString = encodeURIComponent(str).replaceAll('%C2%A0', '%20');
@@ -31,85 +30,75 @@
         return win1255EncodedString.replace(/(%20){2,}/g, '%20');
     }
 
-    // Get TvShow Title To Search In FaveZ0ne
-    var tvshow_title = document.evaluate(
-        "//div[@class='blokl1']/h1[1]",
-        document,
-        null,
-        XPathResult.STRING_TYPE,
-        null
-    ).stringValue.trim();
-
-    // Add New CSS Rules - For The FaveZ0ne Links
-    var sheet = (function() {
-      // Create the <style> tag
-      var style = document.createElement("style");
-      style.appendChild(document.createTextNode(""));
-      // Add the <style> element to the page
-      document.head.appendChild(style);
-      return style.sheet;
-    })();
-    sheet.insertRule(
-        '#pagetext {' +
-            'direction: rtl; ' +
-            'background: url("http://www.favez0ne.net/images/dots_bg2.gif") repeat scroll 100% center rgba(0, 0, 0, 0); ' +
-            'height: auto; ' +
-            'min-height: 580px; ' +
-            'padding: 10px 0px; ' +
-            'text-align: right; ' +
-            'width: 670px; ' +
-            'margin-right: 10px;}',
-        sheet.cssRules.length
-    );
-
-    // inject setDis Function (From FaveZ0ne)
-    var setDis = document.createElement('script');
-    setDis.type="text/javascript";
-    setDis.innerHTML = [
-        "function setDis(objId) {",
-        "    var obj = document.getElementById(objId);",
-        "    obj.style.display = (!obj.style.display) ? 'none': '';",
-        "}",
-        "var toggle = setDis;"
-    ].join("\n");
-    document.head.appendChild(setDis);
+    // Get TvShow Title Encoded To Search In FaveZ0ne (Windows-1255 Encoded)
+    function getWin1255EncodedTvshowTitle() {
+        var tvshow_title = document.evaluate(
+            "//div[@class='blokl1']/h1[1]",
+            document,
+            null,
+            XPathResult.STRING_TYPE,
+            null
+        ).stringValue.trim();
+        return encodeWin1255URIComponent(tvshow_title);
+    }
 
     // Get FaveZ0ne Links Page
-    var response = GM_xmlhttpRequest({
+    GM_xmlhttpRequest({
         method: "POST",
         url: "http://www.favez0ne.net/search.php",
         overrideMimeType: "text/html; charset=windows-1255",
-        data: "srch=" + encodeWin1255URIComponent(tvshow_title),
-        synchronous: true,
+        data: "srch=" + getWin1255EncodedTvshowTitle(),
+        onload: (function(response) {
+            // inject setDis Function (FaveZ0ne Function)
+            var setDis = document.createElement('script');
+            setDis.type="text/javascript";
+            setDis.innerHTML = [
+                "function setDis(objId) {",
+                "    var obj = document.getElementById(objId);",
+                "    obj.style.display = (!obj.style.display) ? 'none': '';",
+                "}",
+                "var toggle = setDis;"
+            ].join("\n");
+            document.head.appendChild(setDis);
+
+            // Set FaveZ0ne Links Instead Of FaveZ0ne Advertise And Remove adf.ly ads
+            var pageText = (function () {
+                var blokl1 = document.evaluate(
+                    "//div[@class='blokl1']/div[contains(., 'FaveZ0ne')]",
+                    document,
+                    null,
+                    XPathResult.FIRST_ORDERED_NODE_TYPE,
+                    null
+                ).singleNodeValue;
+                blokl1.id = "pagetext";
+
+                // Set The Style
+                var style = blokl1.style;
+                style.padding = "10px";
+                style.direction = 'rtl';
+                style.background ='url("http://www.favez0ne.net/images/dots_bg2.gif") repeat 100%';
+                style.height = "auto";
+                style.minHeight = "580px";
+                style.textAlign = "right";
+                style.width = "640px";
+
+                return blokl1;
+            })();
+
+            pageText.innerHTML = (function () {
+                var faveZ0neDoc = new DOMParser().parseFromString(response.responseText, "text/html");
+                var faveZ0nePageText = faveZ0neDoc.getElementById("pagetext");
+                // Remove Results Found Annoying Text
+                faveZ0nePageText.getElementsByTagName('div')[0].remove();
+
+                return faveZ0nePageText.innerHTML.replaceAll(
+                    /http(s)?:\/\/adf\.ly\/\d+\//g,
+                    ""
+                );
+            })();
+        }),
         headers: {
             "Content-Type": "application/x-www-form-urlencoded"
         }
     });
-
-    // Set FaveZ0ne Links Instead Of FaveZ0ne Advertise And Remove adf.ly ads
-    var pageText = (function () {
-        var blokl1 = document.evaluate(
-            "//div[@class='blokl1']/div[contains(., 'FaveZ0ne')]",
-            document,
-            null,
-            XPathResult.FIRST_ORDERED_NODE_TYPE,
-            null
-        ).singleNodeValue;
-        blokl1.id = "pagetext";
-        blokl1.style = "";
-
-        return blokl1;
-    })();
-
-    pageText.innerHTML = (function () {
-        var faveZ0neDoc = new DOMParser().parseFromString(response.responseText, "text/html");
-        var faveZ0nePageText = faveZ0neDoc.getElementById("pagetext");
-        // Remove Results Found Annoying Text
-        faveZ0nePageText.getElementsByTagName('div')[0].remove();
-
-        return faveZ0nePageText.innerHTML.replaceAll(
-            /http(s)?:\/\/adf\.ly\/\d+\//g,
-            ""
-        );
-    })();
 })();
